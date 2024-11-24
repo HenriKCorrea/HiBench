f5365ae618894cebd83cafbb7cea9c6bd7bf51eb
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-11-24 17:06:44 +0000

    Implementing LLMSparkPageRank using Llama 3.1 70B

--- sparkbench/websearch/src/main/scala/org/apache/spark/examples/LLMSparkPageRank.scala
+++ sparkbench/websearch/src/main/scala/org/apache/spark/examples/LLMSparkPageRank.scala
@@ -1,54 +1,50 @@
 package org.apache.spark.examples
-
 import com.intel.hibench.sparkbench.common.IOCommon
 import org.apache.spark.SparkContext._
 import org.apache.spark.{SparkConf, SparkContext}
-
+/**
+ * Computes the PageRank of URLs from an input file. Input file should
+ * be in format of:
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * ...
+ * where URL and their neighbors are separated by space(s).
+ */
 object LLMSparkPageRank {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
     if (args.length < 2) {
-      Console.err.println("Usage: LLMSparkPageRank <input-file> <output-file> [num-iterations]")
+      System.err.println("Usage: LLMSparkPageRank <input_file> <output_filename> [<iter>]")
       System.exit(1)
     }
-
-    val inputFile = args(0)
-    val outputFile = args(1)
-    var numIterations = 1
-    if (args.length > 2) {
-      numIterations = args(2).toInt
-    }
-
     val sparkConf = new SparkConf().setAppName("ScalaPageRank")
-    val sc = new SparkContext(sparkConf)
-
-    // Read the input text file and create an RDD of URL and neighbor URL pairs
-    val lines = sc.textFile(inputFile)
-    val urlNeighbors = lines.map(line => {
-      val fields = line.split(" ")
-      (fields(0), fields(1))
-    }).cache()
-
-    // Initialize each URL's rank at 1.0
-    val ranks = urlNeighbors.map((url, neighbor) => (url, 1.0))
-
-    // Use a damping factor of 0.85 in the rank calculation, with a random jump factor of 0.15
-    var iteration = 0
-    while (iteration < numIterations) {
-      val contribs = ranks.join(urlNeighbors).map {
-        case ((url, neighbor), (rank, urlNeighbor)) =>
-          (neighbor, rank / 2 + 0.15 * rank / 3)
-      }.reduceByKey(_ + _)
-
-      ranks = contribs.join(ranks).map {
-        case ((url, neighbor), (rankContrib, rank)) =>
-          (url, rank + rankContrib)
-      }.cache()
-
-      iteration += 1
+    val input_path = args(0)
+    val output_path = args(1)
+    val iters = if (args.length > 2) args(2).toInt else 10
+    val ctx = new SparkContext(sparkConf)
+    // Load the input file and map each line to a pair of URLs
+    val links = ctx.textFile(input_path).map(line => {
+      val parts = line.split("\\s+")
+      (parts(0), parts(1))
+    })
+    // Create a set of all unique URLs
+    val urls = links.flatMap(link => Iterator(link._1, link._2)).distinct()
+    // Initialize the ranks for each URL to 1.0
+    var ranks = urls.map(url => (url, 1.0)).reduceByKey((a, b) => a)
+    // Run the PageRank algorithm for the specified number of iterations
+    for (i <- 0 until iters) {
+      // Calculate the number of links for each URL
+      val numLinks = links.map(link => (link._1, 1)).reduceByKey((a, b) => a + b)
+      // Join the links with the ranks and the number of links
+      val contribs = links.join(ranks).join(numLinks).map {
+        case (url, ((link, rank), numLink)) =>
+          (link, rank / numLink)
+      }
+      // Sum up the contributions from all URLs
+      ranks = contribs.reduceByKey((a, b) => a + b).mapValues(value => 0.15 + 0.85 * value)
     }
-
-    // Save the final ranks to an output file
-    val io = new IOCommon(sc)
-    io.save(outputFile, ranks)
+    val io = new IOCommon(ctx)
+    io.save(output_path, ranks)
+    ctx.stop()
   }
 }
